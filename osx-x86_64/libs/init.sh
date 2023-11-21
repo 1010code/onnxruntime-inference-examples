2cb465a0de325b1503768358681605f8cb830bea
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2023-11-21 22:40:18 +0800

    add material

---
+++ osx-x86_64/libs/init.sh
@@ -0,0 +1,3 @@
+wget https://github.com/microsoft/onnxruntime/releases/download/v1.15.1/onnxruntime-osx-x86_64-1.15.1.tgz
+tar xvf onnxruntime-osx-x86_64-1.15.1.tgz
+rm -rf onnxruntime-osx-x86_64-1.15.1.tgz
