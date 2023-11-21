b786b2916119170a8d9d9419875e914d1fa12d41
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2023-11-21 22:46:01 +0800

    modify

---
+++ linux-x64/libs/init.sh
@@ -0,0 +1,3 @@
+wget https://github.com/microsoft/onnxruntime/releases/download/v1.15.1/onnxruntime-linux-x64-1.15.1.tgz
+tar xvf onnxruntime-linux-x64-1.15.1.tgz
+rm -rf onnxruntime-linux-x64-1.15.1.tgz
---
+++ win-x64/libs/init.sh
@@ -0,0 +1,3 @@
+wget https://github.com/microsoft/onnxruntime/releases/download/v1.15.1/onnxruntime-win-x64-1.15.1.zip
+unzip onnxruntime-win-x64-1.15.1.zip
+rm -rf onnxruntime-win-x64-1.15.1.zip
